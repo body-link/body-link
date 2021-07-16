e727498a24729553cf76204d67018b7debe23f7c
Dmitrii Bykov <sukazavr@gmail.com>
2021-07-17 05:41:14 +0700

    Optimize atomValueProjection

--- packages/helpers/src/atom/atomValueProjection.ts
+++ packages/helpers/src/atom/atomValueProjection.ts
@@ -1,8 +1,9 @@
 import { MonoTypeOperatorFunction, Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
 import { Atom } from '../packlets/atom';
 
 export const atomValueProjection =
-  <T1, T2 extends T1>(atom: Atom<T1>): MonoTypeOperatorFunction<T2> =>
-  (source: Observable<T2>) =>
-    source.pipe(tap((n) => atom.set(n)));
+  <T>(atom$: Atom<T>): MonoTypeOperatorFunction<T> =>
+  (source: Observable<T>) =>
+    new Observable<T>((subscriber) => {
+      subscriber.add(source.subscribe((t) => atom$.set(t)));
+    });
