6285ab9910cac19c448304e418bc3c3ca1f77c41
Dmitrii Bykov <sukazavr@gmail.com>
2021-07-17 07:55:39 +0700

    Implement mergeFunctionsInObjects

--- packages/helpers/src/packlets/utils/fn.ts
+++ packages/helpers/src/packlets/utils/fn.ts
@@ -1,8 +1,27 @@
-import { isDefined, TOption } from '@body-link/type-guards';
+import { isDefined, isFunction, TOption } from '@body-link/type-guards';
 
 export const optionalCall = <Argument extends TOption<FnArgument>, FnArgument, FnReturn>(
   fn: (arg: FnArgument) => FnReturn,
   arg: Argument
 ): Argument extends undefined ? undefined : FnReturn => {
   return (isDefined(arg) ? fn(arg) : undefined) as Argument extends undefined ? undefined : FnReturn;
+};
+
+export const mergeFunctionsInObjects = <T1, T2>(a: TOption<T1>, b: TOption<T2>): T1 & T2 => {
+  const result = { ...a, ...b } as T1 & T2;
+  if (isDefined(a) && isDefined(b)) {
+    for (const aKey in a) {
+      if (aKey in b) {
+        const aVal = (a as unknown as Record<string, unknown>)[aKey];
+        const bVal = (b as unknown as Record<string, unknown>)[aKey];
+        if (isFunction(aVal) && isFunction(bVal)) {
+          (result as Record<string, unknown>)[aKey] = function () {
+            aVal.apply(window, arguments);
+            bVal.apply(window, arguments);
+          };
+        }
+      }
+    }
+  }
+  return result;
 };
