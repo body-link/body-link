2a0f3f14b7b67c0e62b4ac372a5fef3cdd525c73
Dmitrii Bykov <sukazavr@gmail.com>
2021-07-28 12:38:14 +0700

    Export new UI components

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,8 +1,24 @@
 export * from './theme';
 
+export * from './components/actions-and-inputs/Input/Input';
+export * from './components/actions-and-inputs/InputContainer/InputContainer';
+export * from './components/actions-and-inputs/InputTransparent/InputTransparent';
+export * from './components/actions-and-inputs/ButtonTransparent/ButtonTransparent';
+
+export * from './components/atom-fields/AFInputString/AFInputString';
+
+export * from './components/content/Text/Text';
+
 export * from './components/layout/Box/Box';
 export * from './components/layout/Stack/Stack';
+
 export * from './components/status-and-feedback/Spinner/Spinner';
 
 export * from './components/utils/events';
 export * from './components/utils/overlay';
+
+export * from './components/icons/IconClear';
+export * from './components/icons/IconError';
+export * from './components/icons/IconMinus';
+export * from './components/icons/IconPlus';
+export * from './components/icons/IconChevron';
