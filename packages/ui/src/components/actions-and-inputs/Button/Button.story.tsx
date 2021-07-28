f2f768b6447f073cc9b696c37bec6a2e1c3038a7
Dmitrii Bykov <sukazavr@gmail.com>
2021-07-28 21:19:53 +0700

    Describe Button component variants

--- packages/ui/src/components/actions-and-inputs/Button/Button.story.tsx
+++ packages/ui/src/components/actions-and-inputs/Button/Button.story.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { Button, IPropsButton, EButtonVariant } from './Button';
-import { Stack } from '../../../../lib';
+import { Button, EButtonVariant, IPropsButton } from './Button';
 import { IconPlus } from '../../icons/IconPlus';
+import { Box } from '../../layout/Box/Box';
+import { Text } from '../../content/Text/Text';
+import { Stack } from '../../layout/Stack/Stack';
+import { EThemeFont } from '../../../theme';
 
 export default {
   title: 'Actions & Inputs/Button',
@@ -12,6 +15,7 @@ export default {
     icon: { control: false },
     isFit: { control: false },
     isDisabled: { control: false },
+    isLoading: { control: false },
     ref: { control: false },
   },
 } as Meta;
@@ -52,6 +56,19 @@ const Basic: Story<IPropsButton> = (props) => {
           Button isLoading isDisabled
         </Button>
       </Stack>
+      <Box wMax={58}>
+        <Text variant={EThemeFont.BodyShort}>
+          {props.variant === EButtonVariant.Primary
+            ? 'For the principal call to action on the page. Primary buttons should only appear once per screen (not including the application header, modal dialog, or side panel).'
+            : props.variant === EButtonVariant.Secondary
+            ? 'For secondary actions on each page. Secondary buttons can only be used in conjunction with a primary button. As part of a pair, the secondary button’s function is to perform the negative action of the set, such as “Cancel” or “Back”. Do not use a secondary button in isolation and do not use a secondary button for a positive action.'
+            : props.variant === EButtonVariant.Tertiary
+            ? 'For less prominent, and sometimes independent, actions. Tertiary buttons can be used in isolation or paired with a primary button when there are multiple calls to action. Tertiary buttons can also be used for sub-tasks on a page where a primary button for the main and final action is present.'
+            : props.variant === EButtonVariant.Subtle
+            ? 'For the least pronounced actions; often used in conjunction with a primary button. In a situation such as a progress flow, a subtle button may be paired with a primary and secondary button set, where the primary button is for forward action, the secondary button is for “Back”, and the subtle button is for “Cancel”.'
+            : 'For actions that could have destructive effects on the user’s data (for example, delete or remove). Danger button has three styles: primary, tertiary, and subtle.'}
+        </Text>
+      </Box>
     </Stack>
   );
 };
