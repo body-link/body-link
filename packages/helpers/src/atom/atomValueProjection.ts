936b941661cf09e6d815dfbbcb9c963a0c92968f
Dmitrii Bykov <sukazavr@gmail.com>
2021-07-30 15:04:45 +0700

    Lift atomValueProjection

--- packages/helpers/src/atom/atomValueProjection.ts
+++ packages/helpers/src/atom/atomValueProjection.ts
@@ -1,9 +1,8 @@
 import { MonoTypeOperatorFunction, Observable } from 'rxjs';
+import { tap } from "rxjs/operators";
 import { Atom } from '../packlets/atom';
 
 export const atomValueProjection =
-  <T>(atom$: Atom<T>): MonoTypeOperatorFunction<T> =>
-  (source: Observable<T>) =>
-    new Observable<T>((subscriber) => {
-      subscriber.add(source.subscribe((t) => atom$.set(t)));
-    });
+  <T>(atom$: Atom<T>): MonoTypeOperatorFunction<T> => (
+    source: Observable<T>
+  ) => source.pipe(tap((n) => atom$.set(n)));
