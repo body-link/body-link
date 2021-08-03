199af6c3b76e9a4ae824e8bc96e28fa46ed56a91
Dmitrii Bykov <sukazavr@gmail.com>
2021-08-03 14:02:27 +0700

    Make useForkRef support TOption<Ref<HTMLElement>>

--- packages/helpers/src/react-hooks/useForkRef.ts
+++ packages/helpers/src/react-hooks/useForkRef.ts
@@ -1,19 +1,19 @@
-import { useMemo, MutableRefObject } from 'react';
+import { MutableRefObject, Ref, RefCallback, useMemo } from 'react';
+import { isFunction, isPresent, TOption } from '@body-link/type-guards';
+
 /**
  * Credit to material-ui for this snippet
  */
 
 // eslint-disable-next-line @rushstack/no-new-null
 export type HTMLElementOrNull = HTMLElement | null;
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type CallbackRef = (node: HTMLElementOrNull) => any;
-export type AnyRef = CallbackRef | MutableRefObject<HTMLElementOrNull>;
+export type AnyRef = TOption<Ref<HTMLElement>>;
 
 function setRef(ref: AnyRef, value: HTMLElementOrNull): void {
-  if (typeof ref === 'function') {
+  if (isFunction(ref)) {
     ref(value);
-  } else if (ref) {
-    ref.current = value;
+  } else if (isPresent(ref)) {
+    (ref as MutableRefObject<HTMLElementOrNull>).current = value;
   }
 }
 
@@ -25,19 +25,20 @@ function setRef(ref: AnyRef, value: HTMLElementOrNull): void {
  * @param refB
  */
 // eslint-disable-next-line @rushstack/no-new-null
-export function useForkRef(refA: AnyRef, refB: AnyRef): CallbackRef | null {
+export function useForkRef(refA: AnyRef, refB: AnyRef): RefCallback<HTMLElement> | null {
   /**
    * This will create a new function if the ref props change and are defined.
    * This means react will call the old forkRef with `null` and the new forkRef
    * with the ref. Cleanup naturally emerges from this behavior
    */
-  return useMemo(() => {
-    if (refA === null && refB === null) {
-      return null;
-    }
-    return (refValue: HTMLElementOrNull) => {
-      setRef(refA, refValue);
-      setRef(refB, refValue);
-    };
-  }, [refA, refB]);
+  return useMemo(
+    () =>
+      isPresent(refA) || isPresent(refB)
+        ? (refValue: HTMLElementOrNull) => {
+            setRef(refA, refValue);
+            setRef(refB, refValue);
+          }
+        : null,
+    [refA, refB]
+  );
 }
